872d9baedc9eaf5d202d1bdab92983be4b6462d5
THCGaming <geogich961@gmail.com>
2026-09-23 21:18:54 +0700

    chore: bump beta to V17.1.0-beta.2

--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 return [
     'name' => 'TMS OS',
     'short_name' => 'TMS OS',
-    'build' => 'Platform V17.1.0-beta.1',
+    'build' => 'Platform V17.1.0-beta.2',
     'channel' => 'beta',
     'timezone' => 'Asia/Ho_Chi_Minh',
 ];
